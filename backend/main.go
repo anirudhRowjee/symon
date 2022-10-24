5c2b778e97141d5068b5b7f110256dee9ba49755
Anirudh Rowjee <ani.rowjee@gmail.com>
2022-10-24 20:27:05 +0530

    Added basic loop to fetch metrics, logo, and communication struct

--- backend/main.go
+++ backend/main.go
@@ -11,8 +11,47 @@ import (
 
 // Define the standard tick rate of 500 ms
 const TICK_RATE = time.Millisecond * 500
+const BANNER = `
+  /$$$$$$            /$$      /$$                                     
+ /$$__  $$          | $$$    /$$$                                     
+| $$  \__/ /$$   /$$| $$$$  /$$$$  /$$$$$$  /$$$$$$$                  
+|  $$$$$$ | $$  | $$| $$ $$/$$ $$ /$$__  $$| $$__  $$                 
+ \____  $$| $$  | $$| $$  $$$| $$| $$  \ $$| $$  \ $$                 
+ /$$  \ $$| $$  | $$| $$\  $ | $$| $$  | $$| $$  | $$                 
+|  $$$$$$/|  $$$$$$$| $$ \/  | $$|  $$$$$$/| $$  | $$                 
+ \______/  \____  $$|__/     |__/ \______/ |__/  |__/                 
+           /$$  | $$                                                  
+          |  $$$$$$/                                                  
+           \______/                                                   
+ /$$                           /$$                                 /$$
+| $$                          | $$                                | $$
+| $$$$$$$   /$$$$$$   /$$$$$$$| $$   /$$  /$$$$$$  /$$$$$$$   /$$$$$$$
+| $$__  $$ |____  $$ /$$_____/| $$  /$$/ /$$__  $$| $$__  $$ /$$__  $$
+| $$  \ $$  /$$$$$$$| $$      | $$$$$$/ | $$$$$$$$| $$  \ $$| $$  | $$
+| $$  | $$ /$$__  $$| $$      | $$_  $$ | $$_____/| $$  | $$| $$  | $$
+| $$$$$$$/|  $$$$$$$|  $$$$$$$| $$ \  $$|  $$$$$$$| $$  | $$|  $$$$$$$
+|_______/  \_______/ \_______/|__/  \__/ \_______/|__/  |__/ \_______/
+`
+
+// Define the shape of data being shared between the backend
+// and the frontend
+
+type metricsDashboard struct {
+
+	// --- CPU Information
+	CPU_counts           int       `json:"cpu_count"`            // number of CPUs present
+	CPU_cores            int       `json:"cpu_cores"`            // number of cores
+	CPU_usage_percentage []float64 `json:"cpu_usage_percentage"` // usage statistics
+
+	// --- Memory Information
+	MEM_usage_percentage float64 `json:"mem_usage_percentage"` // usage statistics
+	MEM_total_available  int     `json:"mem_total_available"`  // all the available memory
+	MEM_total_used       int     `json:"mem_total_used"`       // how much of it is used
+}
 
 func main() {
+
+	fmt.Println(BANNER)
 
 	// TODO Declare shared state
 
